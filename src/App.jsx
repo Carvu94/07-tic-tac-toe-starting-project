a3dd3d616733ef0f237f0f79b0d23f72c9d4989f
Matej Car <74019532+Carvu94@users.noreply.github.com>
2024-04-10 17:18:51 +0100

    setup base gameboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player"
+import GameBoard from "./components/Gameboard"
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
           <Player initialName="Player 2" symbol="O" />
         </ol>
 
-        Game board
+        <GameBoard />
       </div>
 
       log
